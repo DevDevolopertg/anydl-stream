78305bf6685f513d8b4cd46dd0a7f4f3d245d1fc
amalpm615 <52080988+amalpm615@users.noreply.github.com>
2020-05-12 13:19:42 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,7 +1,7 @@
 class Translation(object):
     START_TEXT = """<b>Welcome to Tg_Stream Bot.</b>
 
-😇Simple bot for Convert Telegram Media file to Streaming Video with Custom Thumbnail Support😘😘
+😇 Simple bot for Convert Telegram Media file to Streaming Video with Custom Thumbnail Support 😘😘
   
   ☛ <b> Do one By One . Otherwise you will get Permenent Ban </b>
   **©** **@Amal_PM**
