27e358787a0799dfd422ffe3db031db1af3d4ab1
amalpm615 <52080988+amalpm615@users.noreply.github.com>
2020-05-12 12:20:50 +0530

    Update convert_to_video.py

--- plugins/convert_to_video.py
+++ plugins/convert_to_video.py
@@ -64,13 +64,13 @@ async def convert_to_video(bot, update):
             )
         )
         if the_real_download_location is not None:
-            await bot.edit_message_text(
+            bot.edit_message_text(
                 text=Translation.SAVED_RECVD_DOC_FILE,
                 chat_id=update.chat.id,
                 message_id=a.message_id
             )
             # don't care about the extension
-            await bot.edit_message_text(
+            bot.edit_message_text(
                 text=Translation.UPLOAD_START,
                 chat_id=update.chat.id,
                 message_id=a.message_id
